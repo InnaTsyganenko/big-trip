43f516d14eda2588774abd208a8101b32b21acc3
Nikulichka <poteryaevainna@gmail.com>
2021-05-01 17:41:38 +0300

    Added logick of viewing of edit form

--- src/main.js
+++ src/main.js
@@ -17,7 +17,6 @@ const siteHeaderContainerElement = document.querySelector('.page-header__contain
 const siteTripInfoElement = siteHeaderContainerElement.querySelector('.trip-main');
 const siteNavigationElement = siteHeaderContainerElement.querySelector('.trip-controls__navigation');
 const siteFiltersElement = siteHeaderContainerElement.querySelector('.trip-controls__filters');
-const addPointButton = siteHeaderContainerElement.querySelector('.trip-main__event-add-btn');
 
 const body = document.querySelector('.page-body');
 const siteMainElement = document.querySelector('.page-main');
@@ -47,7 +46,7 @@ const renderPoint = (pointListElement, point) => {
   };
 
   const onEscKeyDown = (evt) => {
-    if (evt.key === 'Escape' || evt.key === 'Esc') {
+    if (isEscEvent(evt)) {
       evt.preventDefault();
       replaceFormToCard();
       document.removeEventListener('keydown', onEscKeyDown);
@@ -55,8 +54,16 @@ const renderPoint = (pointListElement, point) => {
   };
 
   pointComponent.getElement().querySelector('.event__rollup-btn').addEventListener('click', () => {
+    if (pointListElement.querySelector('.event--edit')) {
+      pointListElement.querySelector('.event--edit').querySelector('.event__reset-btn').click();
+    }
     replaceCardToForm();
     document.addEventListener('keydown', onEscKeyDown);
+  });
+
+  pointEditComponent.getElement().querySelector('.event__rollup-btn').addEventListener('click', () => {
+    replaceFormToCard();
+    document.removeEventListener('keydown', onEscKeyDown);
   });
 
   pointEditComponent.getElement().querySelector('form').addEventListener('submit', (evt) => {
@@ -76,29 +83,52 @@ const renderPoint = (pointListElement, point) => {
 
 sortingDatePointsSlice.forEach((listPoint) => renderPoint(pointListComponent.getElement(), listPoint));
 
-const closeAddForm = (evt) => {
-  if (isEscEvent(evt)) {
-    evt.preventDefault();
+const renderAddForm = (addForm) => {
+  const addPointButton = siteHeaderContainerElement.querySelector('.trip-main__event-add-btn');
+  addForm = new Array(1).fill().map(generatePoint);
+  const pointAddComponent = new AddPointView(addForm[0]);
+
+  const showAddForm = () => {
+    if (pointListComponent.getElement().querySelector('.event--edit')) {
+      pointListComponent.getElement().querySelector('.event--edit').querySelector('.event__reset-btn').click();
+    }
+    filterEverythingInput.checked = true;
+    sortDayInput.checked = true;
+    render(pointListComponent.getElement(), pointAddComponent.getElement(), RenderPosition.AFTERBEGIN);
+    addPointButton.disabled = true;
+  };
+
+  const closeAddForm = () => {
     pointListComponent.getElement().querySelector('.event--edit').remove();
     addPointButton.disabled = false;
-    body.removeEventListener('keydown', closeAddForm);
-  }
+  };
+
+  const onEscKeyDown = (evt) => {
+    if (isEscEvent(evt)) {
+      evt.preventDefault();
+      closeAddForm();
+      body.removeEventListener('keydown', onEscKeyDown);
+    }
+  };
+
+  addPointButton.addEventListener('click', () => {
+    showAddForm();
+    body.addEventListener('keydown', onEscKeyDown);
+  });
+
+  pointAddComponent.getElement().querySelector('.event__reset-btn').addEventListener('click', () => {
+    closeAddForm();
+  });
 };
 
-const addPoint = new Array(1).fill().map(generatePoint);
-
-addPointButton.addEventListener('click', () => {
-  filterEverythingInput.checked = true;
-  sortDayInput.checked = true;
-  render(pointListComponent.getElement(), new AddPointView(addPoint[0]).getElement(), RenderPosition.AFTERBEGIN);
-  addPointButton.disabled = true;
-  body.addEventListener('keydown', closeAddForm);
-});
+renderAddForm();
 
 const siteFiltersElements = siteHeaderContainerElement.querySelectorAll('.trip-filters__filter-input');
 const sitePointsSortElements = siteMainElement.querySelectorAll('.trip-sort__input');
 const filterEverythingInput = siteHeaderContainerElement.querySelector('#filter-everything');
 const sortDayInput = siteTripEventsElement.querySelector('#sort-day');
+const sortTimeInput = siteTripEventsElement.querySelector('#sort-time');
+const sortPriceInput = siteTripEventsElement.querySelector('#sort-price');
 
 const isFutureOrCurrentPoints = () => {
   return sortingDatePointsSlice.filter((point) => (point.datetimeStart.isSameOrAfter(dayjs(), 'm'))
@@ -125,6 +155,7 @@ siteFiltersElements.forEach((filterElement) => {
       ||
       filterElement.id === 'filter-everything' ? sortingDatePointsSlice : false;
     arrfiltredPoints = filtredPoints();
+    (arrfiltredPoints.length < '2') ? ((sortTimeInput.disabled = true) & (sortPriceInput.disabled = true)) : ((sortTimeInput.disabled = false) & (sortPriceInput.disabled = false));
     filtredPoints().forEach((listPoint) => renderPoint(pointListComponent.getElement(), listPoint));
   });
 });
@@ -132,16 +163,14 @@ siteFiltersElements.forEach((filterElement) => {
 sitePointsSortElements.forEach((sortElement) => {
   sortElement.addEventListener('change', () => {
     pointListComponent.getElement().innerHTML = '';
-    if (sortingDatePointsSlice > '1') {
-      const sortedPoints = arrfiltredPoints.sort((a, b) =>
-        ((sortElement.id === 'sort-time') ? (b.duration - a.duration) : false)
-        ||
-        (sortElement.id === 'sort-price' ? (b.price - a.price) : false)
-        ||
-        (sortElement.id === 'sort-everything'
-          ? sortingDatePointsSlice : false),
-      );
-      sortedPoints.forEach((listPoint) => renderPoint(pointListComponent.getElement(), listPoint));
-    }
+    const sortedPoints = arrfiltredPoints.sort((a, b) =>
+      ((sortElement.id === 'sort-time') ? (b.duration - a.duration) : false)
+      ||
+      (sortElement.id === 'sort-price' ? (b.price - a.price) : false)
+      ||
+      (sortElement.id === 'sort-everything'
+        ? sortingDatePointsSlice : false),
+    );
+    sortedPoints.forEach((listPoint) => renderPoint(pointListComponent.getElement(), listPoint));
   });
 });
